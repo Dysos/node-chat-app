5b989db6a11ffa4076dd5b19e7ad77d92cf25160
Victor Gro <victor@victorgro.com>
2018-10-12 15:49:41 +0200

    Greet new user and alert other users

--- server/server.js
+++ server/server.js
@@ -16,13 +16,32 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
     console.log("New user connected");
 
+    socket.emit("newMessage", {
+        from: "Admin",
+        text: "Welcome the server",
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit("newMessage", {
+        from: "Admin",
+        text: "A new player has joined the server",
+        createdAt: new Date().getTime()
+    });
+
+    //socket.emit from admin text welcome to the chat app
+    //socket.broadcast.emit from admin text new user joined
+
     socket.on("createMessage", (message) => {
-        console.log("New Message: ", message);
+        //console.log("New Message: ", message);
         io.emit("newMessage", {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+       /* socket.broadcast.emit("newMessage", {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });*/
     });
 
     socket.on("disconnect", () => {
