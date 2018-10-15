bde657fd4f3e7912be62028f32422392cf4beb21
Victor Gro <victor@victorgro.com>
2018-10-15 23:28:55 +0200

    Fixed chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -99,7 +99,6 @@ const socket = io();
             const messageTextbox = jQuery("[name=message]");
 
             socket.emit("createMessage", {
-                from: "User",
                 text: messageTextbox.val()
             }, function() {
                 messageTextbox.val("");
